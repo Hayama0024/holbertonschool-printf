fc1aadf64c2d77d43377c4f00a77b5ac565fa208
Hayama0024 <hayamareizo11@gmail.com>
2025-03-24 23:11:04 +0000

    Merged main.h from feature/Ryota

--- main.h
+++ main.h
@@ -1,6 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
+/*structure*/
+/*
+struct print - prints char,string,%
+@type: type de variable
+@f: the function associated
+*/
+
+typedef struct print
+{
+	char *type;
+	void (*f)(va_list ap);
+}print_type;
+
 
 /*prototype*/
 
