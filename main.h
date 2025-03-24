f3df48bf542e6005ecb180d5da36bde995d5767b
Emi-H106 <emi.hatano@yahoo.com>
2025-03-24 09:24:49 +0000

    create main.h

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+/*structure*/
+/*
+struct print - prints char,string,%
+@type: type de variable
+@f: the function associated
+*/
+
+typedef struct print
+{
+	char *type;
+	void (*f)(va_list ap);
+}print_type;
+
+
+/*prototype*/
+
+int _printf(const char *format, ...);
+
+#endif
